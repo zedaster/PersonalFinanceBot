fe9d232d13f658b5151f62db2f7b8bdcb7a149d0
sxkxixx <sasha.kornilov.1212@gmail.com>
2023-11-04 19:45:38 +0500

    Initialize empty project

---
+++ src/test/java/ru/naumen/personalfinancebot/PeronalFinanceBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.naumen.personalfinancebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeronalFinanceBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
