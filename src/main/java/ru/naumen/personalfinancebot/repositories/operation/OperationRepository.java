1eb551dfb9c9706195bfaa8b17470c54542bee29
sxkxixx <sasha.kornilov.1212@gmail.com>
2023-11-27 16:26:54 +0500

    Переписал обработчик для команды '/report_expense' и написал тесты для этого обработчика

--- src/main/java/ru/naumen/personalfinancebot/repositories/operation/OperationRepository.java
+++ src/main/java/ru/naumen/personalfinancebot/repositories/operation/OperationRepository.java
@@ -12,7 +12,22 @@ import java.util.Map;
  * Интерфейс репозитория модели данных "операция"
  */
 public interface OperationRepository {
+    /**
+     * Добавляет запись операции в базу данных и возвращаёт её
+     * @param user Пользователь
+     * @param category Категория
+     * @param payment Плата
+     * @return Добавленная операция
+     */
     Operation addOperation(User user, Category category, double payment);
 
+    /**
+     * Возвращает MAP, где ключ - название каатегории, значение - сумма операций по данной категории
+     * @param user Пользователь
+     * @param month Месяц
+     * @param year Год
+     * @param type Тип операции
+     * @return Словарь с операциями
+     */
     Map<String, Double> getOperationsSumByType(User user, int month, int year, CategoryType type);
 }
