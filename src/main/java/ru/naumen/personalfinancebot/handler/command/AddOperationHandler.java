528fab2401ff96c731a13cb57640c6b197693387
sxkxixx <sasha.kornilov.1212@gmail.com>
2023-12-13 01:31:25 +0500

    Исправил добавление операции по категори, состаящей из нескольких слов + тесты для данного случая

--- src/main/java/ru/naumen/personalfinancebot/handler/command/AddOperationHandler.java
+++ src/main/java/ru/naumen/personalfinancebot/handler/command/AddOperationHandler.java
@@ -10,6 +10,7 @@ import ru.naumen.personalfinancebot.model.User;
 import ru.naumen.personalfinancebot.repository.category.CategoryRepository;
 import ru.naumen.personalfinancebot.repository.operation.OperationRepository;
 import ru.naumen.personalfinancebot.repository.user.UserRepository;
+import ru.naumen.personalfinancebot.service.ArgumentParseService;
 
 import java.util.List;
 import java.util.Optional;
@@ -40,12 +41,18 @@ public class AddOperationHandler implements CommandHandler {
      */
     private final OperationRepository operationRepository;
 
+    /**
+     * Сервис, который парсит аргументы
+     */
+    private final ArgumentParseService argumentParser;
+
     public AddOperationHandler(CategoryType categoryType, UserRepository userRepository,
-                               CategoryRepository categoryRepository, OperationRepository operationRepository) {
+                               CategoryRepository categoryRepository, OperationRepository operationRepository, ArgumentParseService argumentParser) {
         this.categoryType = categoryType;
         this.userRepository = userRepository;
         this.categoryRepository = categoryRepository;
         this.operationRepository = operationRepository;
+        this.argumentParser = argumentParser;
     }
 
     /**
@@ -53,7 +60,7 @@ public class AddOperationHandler implements CommandHandler {
      */
     @Override
     public void handleCommand(CommandData commandData, Session session) {
-        if (commandData.getArgs().size() != 2) {
+        if (commandData.getArgs().size() < 2) {
             commandData.getBot().sendMessage(commandData.getUser(), Message.INCORRECT_OPERATION_ARGS_AMOUNT);
             return;
         }
@@ -96,7 +103,7 @@ public class AddOperationHandler implements CommandHandler {
         if (payment <= 0) {
             throw new IllegalArgumentException();
         }
-        String categoryName = args.get(1);
+        String categoryName = this.argumentParser.parseCategory(args.subList(1, args.size()));
         if (type == CategoryType.EXPENSE) {
             payment = -Math.abs(payment);
         } else if (type == CategoryType.INCOME) {
