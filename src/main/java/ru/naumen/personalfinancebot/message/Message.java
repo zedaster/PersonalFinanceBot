cadb850d17643ed61adf01ccea91b703ab69dd91
sxkxixx <sasha.kornilov.1212@gmail.com>
2023-12-14 20:48:55 +0500

    Написал JavaDoc для сообщений, у которых не было JavaDoc

--- src/main/java/ru/naumen/personalfinancebot/message/Message.java
+++ src/main/java/ru/naumen/personalfinancebot/message/Message.java
@@ -102,8 +102,8 @@ public class Message {
      * Сообщение о неверно переданной дате (месяц и год) для команды /report_expense
      */
     public static final String INCORRECT_SELF_REPORT_VALUES = """
-    Переданы неверные данные месяца и года.
-    Дата должна быть передана в виде "MM.YYYY", например, "11.2023".""";
+            Переданы неверные данные месяца и года.
+            Дата должна быть передана в виде "MM.YYYY", например, "11.2023".""";
 
     /**
      * Начало отчета по расходам пользователя
@@ -125,12 +125,25 @@ public class Message {
      */
     public static final String EXPENSE_REPORT_PATTERN = "{category}: {payment} руб.\n";
 
+    /**
+     * Сообщение о неверно введённой дате (месяц и год)
+     */
     public static final String INCORRECT_BUDGET_YEAR_MONTH = "Дата введена неверно! Введите ее в формате " +
             "[mm.yyyy - месяц.год]";
 
+    /**
+     * Сообщение о некорректно переданной сумме бюджета при его создании
+     */
     public static final String INCORRECT_BUDGET_NUMBER_ARG = "Все суммы должны быть больше нуля!";
+
+    /**
+     * Сообщение о некорректно переданном годе для созданию бюджета.
+     */
     public static final String INCORRECT_BUDGET_YEAR_ARG = "Год введен неверно! Дальше 3000 года не стоит планировать.";
 
+    /**
+     * Сообщение, предоставляющую пользователю информацию о доступных командах для пользователя
+     */
     public static final String BUDGET_HELP = """
             Доступные команды для работы с бюджетами:
             /budget - показать бюджет за текущий месяц
@@ -139,11 +152,20 @@ public class Message {
             /budget_list [mm.yyyy from - месяц от] [mm.year to - месяц до] - запланированные бюджеты за определенные месяца
             /budget_create [mm.yyyy - месяц.год] [ожидаемый доход] [ожидаемый расходы] - планировать бюджет""";
 
+    /**
+     * Сообщение о неверно введенной команде /budget_create
+     */
     public static final String INCORRECT_CREATE_BUDGET_ENTIRE_ARGS = "Неверно введена команда! Введите " +
             "/budget_create [mm.yyyy - месяц.год] [ожидаемый доход] [ожидаемый расходы]";
 
+    /**
+     * Сообщение об ошибке в случае если пользователь планирует бюджет на прошлое.
+     */
     public static final String CANT_CREATE_OLD_BUDGET = "Вы не можете создавать бюджеты за прошедшие месяцы!";
 
+    /**
+     * Шаблон сообщения для вывода сообщения о созданном бюджете
+     */
     public static final String BUDGET_CREATED = """
             Бюджет на {month} {year} создан.
             Ожидаемые доходы: {expect_income}
@@ -154,48 +176,90 @@ public class Message {
             Нужно еще заработать: {income_left}
             Еще осталось на траты: {expenses_left}""";
 
+    /**
+     * Сообщение о неверно введенной команде при редактировании бюджета
+     */
     public static final String INCORRECT_EDIT_BUDGET_ENTIRE_ARGS = "Неверно введена команда! Введите " +
             "/budget_set_[income/expenses] [mm.yyyy - месяц.год] [ожидаемый доход/расход]";
 
+    /**
+     * Сообщение об отсутсвии бюджета на указанную пользователем дату
+     */
     public static final String BUDGET_NOT_FOUND = "Бюджет на этот период не найден! Создайте его командой " +
             "/budget_create [mm.yyyy - месяц.год] [ожидаемый доход] [ожидаемый расходы]";
 
+    /**
+     * Сообщение об ошибке при реадктировании бюджета за прошедние месяцы
+     */
     public static final String CANT_EDIT_OLD_BUDGET = "Вы не можете изменять бюджеты за прошедшие месяцы!";
 
+    /**
+     * Шаблон сообщение при успещном редактировании бюджета
+     */
     public static final String BUDGET_EDITED = """
             Бюджет на {month} {year} изменен:
             Ожидаемые доходы: {expect_income}
             Ожидаемые расходы: {expect_expenses}""";
 
+    /**
+     * Сообщение о неверно введенной команде /budget_list
+     */
     public static final String INCORRECT_LIST_BUDGET_ENTIRE_ARGS = """
             Неверно введена команда! Введите
             или /budget_list - вывод бюджетов за 12 месяцев (текущий + предыдущие),
             или /budget_list [год] - вывод бюджетов за определенный год,
             или /budget_list [mm.yyyy - месяц.год] [mm.yyyy - месяц.год] - вывод бюджетов за указанный промежуток.""";
 
+    /**
+     * Сообщение об ошибке если передал дату начала, которая позднее даты конца
+     */
     public static final String BUDGET_LIST_FROM_IS_AFTER_TO = "Дата начала не может быть позднее даты конца периода!";
 
+    /**
+     * Префикс для спискаа запланированных бюджетов.
+     */
     public static final String BUDGET_LIST_PREFIX = "Ваши запланированные доходы и расходы по месяцам:";
 
+    /**
+     * Шаблон для вывода бюджета за конкретный год и месяц
+     */
     public static final String BUDGET_LIST_ELEMENT = """
             {month} {year}:
             Ожидание: + {expect_income} | - {expect_expenses}
             Реальность: + {real_income} | - {real_expenses}""";
 
+    /**
+     * Постфикс для сообщения пользователю при выводе списка бюджетов за n-ое кол-во месяцев
+     */
     public static final String BUDGET_LIST_RANGE_POSTFIX = "Данные показаны за {count} месяц(-ев).";
 
+    /**
+     * Постфикс для сообщения пользователю при выводе списка бюджетов за конкретный год
+     */
     public static final String BUDGET_LIST_YEAR_POSTFIX = "Данные показаны за {year} год.";
 
+    /**
+     * Сообщение для вывода списка бюджетов за последние 12 месяцев.
+     */
     public static final String BUDGET_LIST_TWELVE_MONTHS_POSTFIX = "Данные показаны за последние 12 месяцев. " +
             "Чтобы посмотреть данные, например, за 2022, введите /budget_list 2022.\n" +
             "Для показа данных по определенным месяцам, например, с ноября 2022 по январь 2023 введите " +
             "/budget_list 10.2022 01.2023";
 
+    /**
+     * Сообщение об отсутствии бюджетов за указанный пользователем период
+     */
     public static final String BUDGET_LIST_EMPTY = "У вас не было бюджетов за этот период. Для создания бюджета " +
             "введите /budget_create [mm.yyyy - месяц.год] [ожидаемый доход] [ожидаемый расходы]";
 
+    /**
+     * Сообщение об отсутсвии бюджета за конкретную дату
+     */
     public static final String CURRENT_BUDGET_NOT_EXISTS = "Бюджет на {month} {year} отсутствует";
 
+    /**
+     * Шаблон сообщения для вывода текущего бюджета
+     */
     public static final String CURRENT_BUDGET = """
             Бюджет на {month} {year}:
             Ожидаемые доходы: {expect_income}
